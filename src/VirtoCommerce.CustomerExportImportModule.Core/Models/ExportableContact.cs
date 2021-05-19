9f662c9c93b554509d1a9e38fca81eeb095e556f
Anton Veretennikov <superiorboroda@gmail.com>
2021-05-19 17:22:07 +0300

    Use base class for contacts and orgs

--- src/VirtoCommerce.CustomerExportImportModule.Core/Models/ExportableContact.cs
+++ src/VirtoCommerce.CustomerExportImportModule.Core/Models/ExportableContact.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using CsvHelper.Configuration.Attributes;
 using VirtoCommerce.CustomerModule.Core.Model;
@@ -8,86 +7,85 @@ using VirtoCommerce.StoreModule.Core.Model;
 
 namespace VirtoCommerce.CustomerExportImportModule.Core.Models
 {
-    public sealed class ExportableContact : IExportable, IHasDynamicProperties
+    public sealed class ExportableContact : ExportableMember
     {
         [Name("Contact Id")]
-        public string Id { get; set; }
+        public override string Id { get; set; }
+
         [Name("Contact First Name")]
         public string FirstName { get; set; }
+
         [Name("Contact Last Name")]
         public string LastName { get; set; }
+
         [Name("Contact Full Name")]
         public string FullName { get; set; }
+
         [Name("Contact Outer Id")]
         public string ContactOuterId { get; set; }
+
         [Name("Organization Id")]
         public string OrganizationId { get; set; }
+
         [Name("Organization Outer Id")]
         public string OrganizationOuterId { get; set; }
+
         [Name("Organization Name")]
         public string OrganizationName { get; set; }
+
         [Name("Account Id")]
         public string AccountId { get; set; }
+
         [Name("Account Login")]
         public string AccountLogin { get; set; }
+
         [Name("Store Id")]
         public string StoreId { get; set; }
+
         [Name("Store Name")]
         public string StoreName { get; set; }
+
         [Name("Account Email")]
         public string AccountEmail { get; set; }
+
         [Name("Account Type")]
         public string AccountType { get; set; }
+
         [Name("Account Status")]
         public string AccountStatus { get; set; }
+
         [Name("Email Verified")]
         public bool? EmailVerified { get; set; }
+
         [Name("Contact Status")]
         public string ContactStatus { get; set; }
+
         [Name("Associated Organization Id")]
         public string AssociatedOrganizationId { get; set; }
+
         [Name("Birthday")]
         public DateTime? BirthDate { get; set; }
+
         [Name("TimeZone")]
         public string TimeZone { get; set; }
+
         [Name("Phones")]
         public string Phones { get; set; }
+
         [Name("User groups")]
         public string UserGroups { get; set; }
+
         [Name("Default language")]
         public string DefaultLanguage { get; set; }
+
         [Name("Taxpayer ID")]
         public string TaxPayerId { get; set; }
+
         [Name("Preferred communication")]
         public string PreferredCommunication { get; set; }
+
         [Name("Preferred delivery")]
         public string PreferredDelivery { get; set; }
-        [Name("Address Type")]
-        public string AddressType { get; set; }
-        [Name("Address First Name")]
-        public string AddressFirstName { get; set; }
-        [Name("Address Last Name")]
-        public string AddressLastName { get; set; }
-        [Name("Address Country")]
-        public string AddressCountry { get; set; }
-        [Name("Address Region")]
-        public string AddressRegion { get; set; }
-        [Name("Address City")]
-        public string AddressCity { get; set; }
-        [Name("Address Address Line1")]
-        public string AddressAddressLine1 { get; set; }
-        [Name("Address Address Line2")]
-        public string AddressAddressLine2 { get; set; }
-        [Name("Address Zip Code")]
-        public string AddressZipCode { get; set; }
-        [Name("Address Email")]
-        public string AddressEmail { get; set; }
-        [Name("Address Phone")]
-        public string AddressPhone { get; set; }
-
-        [Ignore]
-        public string ObjectType => typeof(ExportableContact).FullName;
-        public ICollection<DynamicObjectProperty> DynamicProperties { get; set; }
 
         public ExportableContact FromModel(Contact contact, Organization organization, Store store)
         {
@@ -128,11 +126,9 @@ namespace VirtoCommerce.CustomerExportImportModule.Core.Models
                 AddressZipCode = address?.Zip,
                 AddressEmail = address?.Email,
                 AddressPhone = address?.Phone,
-
                 DynamicProperties = contact.DynamicProperties?.Select(x => x.Clone() as DynamicObjectProperty)
                     .ToArray()
             };
-
 
             return result;
         }
--- src/VirtoCommerce.CustomerExportImportModule.Core/Models/ExportableOrganization.cs
+++ src/VirtoCommerce.CustomerExportImportModule.Core/Models/ExportableOrganization.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using System.Linq;
 using CsvHelper.Configuration.Attributes;
 using VirtoCommerce.CustomerModule.Core.Model;
@@ -6,58 +5,40 @@ using VirtoCommerce.Platform.Core.DynamicProperties;
 
 namespace VirtoCommerce.CustomerExportImportModule.Core.Models
 {
-    public sealed class ExportableOrganization : IExportable, IHasDynamicProperties
+    public sealed class ExportableOrganization : ExportableMember
     {
         [Name("Organization Id")]
-        public string Id { get; set; }
+        public override string Id { get; set; }
+
         [Name("Organization Outer Id")]
         public string OrganizationOuterId { get; set; }
+
         [Name("Organization Name")]
         public string OrganizationName { get; set; }
+
         [Name("Parent Organization Name")]
         public string ParentOrganizationName { get; set; }
+
         [Name("Parent Organization Id")]
         public string ParentOrganizationId { get; set; }
+
         [Name("Parent Organization Outer Id")]
         public string ParentOrganizationOuterId { get; set; }
-        [Name("Address Type")]
-        public string AddressType { get; set; }
-        [Name("Address First Name")]
-        public string AddressFirstName { get; set; }
-        [Name("Address Last Name")]
-        public string AddressLastName { get; set; }
-        [Name("Address Country")]
-        public string AddressCountry { get; set; }
-        [Name("Address Region")]
-        public string AddressRegion { get; set; }
-        [Name("Address City")]
-        public string AddressCity { get; set; }
-        [Name("Address Address Line1")]
-        public string AddressAddressLine1 { get; set; }
-        [Name("Address Address Line2")]
-        public string AddressAddressLine2 { get; set; }
-        [Name("Address Zip Code")]
-        public string AddressZipCode { get; set; }
-        [Name("Address Email")]
-        public string AddressEmail { get; set; }
-        [Name("Address Phone")]
-        public string AddressPhone { get; set; }
+
         [Name("Phones")]
         public string Phones { get; set; }
+
         [Name("Business category")]
         public string BusinessCategory { get; set; }
+
         [Name("Description")]
         public string Description { get; set; }
+
         [Name("Organization Groups")]
         public string OrganizationGroups { get; set; }
 
-        [Ignore]
-        public string ObjectType => typeof(ExportableOrganization).FullName;
-        public ICollection<DynamicObjectProperty> DynamicProperties { get; set; }
-
         public ExportableOrganization FromModel(Organization organization, Organization parentOrganization)
         {
-
             var address = organization.Addresses.FirstOrDefault();
 
             var result = new ExportableOrganization
@@ -82,7 +63,6 @@ namespace VirtoCommerce.CustomerExportImportModule.Core.Models
                 BusinessCategory = organization.BusinessCategory,
                 Description = organization.Description,
                 OrganizationGroups = string.Join(", ", organization.Groups),
-
                 DynamicProperties = organization.DynamicProperties?.Select(x => x.Clone() as DynamicObjectProperty)
                     .ToArray()
             };

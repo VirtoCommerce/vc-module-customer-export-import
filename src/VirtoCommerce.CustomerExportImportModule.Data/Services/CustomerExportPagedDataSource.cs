46f3b21970b06ba1cc097c98722c52853bbe5ba6
Aleksandr Vishniakov <av@virtoway.com>
2021-05-19 16:54:06 +0200

    Add parent org and store loading

--- src/VirtoCommerce.CustomerExportImportModule.Data/Services/CustomerExportPagedDataSource.cs
+++ src/VirtoCommerce.CustomerExportImportModule.Data/Services/CustomerExportPagedDataSource.cs
@@ -7,6 +7,8 @@ using VirtoCommerce.CustomerExportImportModule.Core.Services;
 using VirtoCommerce.CustomerModule.Core.Model;
 using VirtoCommerce.CustomerModule.Core.Model.Search;
 using VirtoCommerce.CustomerModule.Core.Services;
+using VirtoCommerce.StoreModule.Core.Model;
+using VirtoCommerce.StoreModule.Core.Services;
 
 namespace VirtoCommerce.CustomerExportImportModule.Data.Services
 {
@@ -14,12 +16,14 @@ namespace VirtoCommerce.CustomerExportImportModule.Data.Services
     {
         private readonly IMemberService _memberService;
         private readonly IMemberSearchService _memberSearchService;
+        private readonly IStoreService _storeService;
         private readonly ExportDataRequest _request;
 
-        public CustomerExportPagedDataSource(IMemberService memberService, IMemberSearchService memberSearchService, int pageSize, ExportDataRequest request)
+        public CustomerExportPagedDataSource(IMemberService memberService, IMemberSearchService memberSearchService, IStoreService storeService, int pageSize, ExportDataRequest request)
         {
             _memberService = memberService;
             _memberSearchService = memberSearchService;
+            _storeService = storeService;
             _request = request;
 
             PageSize = pageSize;
@@ -47,35 +51,57 @@ namespace VirtoCommerce.CustomerExportImportModule.Data.Services
                 return false;
             }
 
+            // Build search criteria
             var searchCriteria = _request.ToSearchCriteria();
             searchCriteria.Skip = (CurrentPageNumber - 1) * PageSize;
             searchCriteria.Take = PageSize;
+            searchCriteria.ResponseGroup = (MemberResponseGroup.Default | MemberResponseGroup.WithAddresses | MemberResponseGroup.WithDynamicProperties |
+                                            MemberResponseGroup.WithEmails | MemberResponseGroup.WithPhones | MemberResponseGroup.WithSecurityAccounts).ToString();
 
+            // Get all exporting members from current chunk
             var searchResult = await _memberSearchService.SearchMembersAsync(searchCriteria);
 
+            // Split members into contacts and organizations
             var contacts = searchResult.Results.Where(member => member.MemberType == nameof(Contact)).Cast<Contact>().ToArray();
             var organizations = searchResult.Results.Where(member => member.MemberType == nameof(Organization)).Cast<Organization>().ToArray();
 
-            var contactOrganizationIds = contacts.SelectMany(contact => contact.Organizations).Distinct().ToArray();
+            // Get IDs of contact & organization parent organization
+            var contactOrganizationIds = contacts.Select(contact => contact.Organizations.OrderBy(organizationId => organizationId).FirstOrDefault()).Distinct().ToArray();
+            var parentOrganizationIds = organizations.Select(organization => organization.ParentId).Distinct().ToArray();
 
-            var loadedOrganizations = organizations.Where(organization => contactOrganizationIds.Contains(organization.Id)).ToArray();
+            // Get already loaded organizations and their IDs
+            var loadedOrganizations = organizations.Where(organization => contactOrganizationIds.Contains(organization.Id) || parentOrganizationIds.Contains(organization.Id)).ToArray();
             var loadedOrganizationIds = loadedOrganizations.Select(organization => organization.Id);
 
-            var notExistOrganizationIds = contactOrganizationIds.Where(contactOrganizationId => loadedOrganizationIds.Contains(contactOrganizationId)).ToArray();
-            var additionalOrganizations = (await _memberService.GetByIdsAsync(notExistOrganizationIds)).Cast<Organization>();
+            // Get not loaded organizations and their IDs
+            var notLoadedOrganizationIds = contactOrganizationIds
+                .Where(contactOrganizationId => loadedOrganizationIds.Contains(contactOrganizationId))
+                .Concat(parentOrganizationIds.Where(parentOrganizationId => loadedOrganizationIds.Contains(parentOrganizationId)))
+                .ToArray();
+            var additionalOrganizations = (await _memberService.GetByIdsAsync(notLoadedOrganizationIds, MemberResponseGroup.Default.ToString())).Cast<Organization>();
 
+            // Get all required organizations
             var allOrganizations = loadedOrganizations.Concat(additionalOrganizations).ToDictionary(x => x.Id, x => x);
+
+            // Get stores from accounts
+            var accounts = contacts.Select(contact => contact.SecurityAccounts.OrderBy(account => account.Id).FirstOrDefault()).Where(account => account != null).Distinct().ToArray();
+            var storeIds = accounts.Select(account => account.StoreId).ToArray();
+            var stores = (await _storeService.GetByIdsAsync(storeIds, StoreResponseGroup.None.ToString())).ToDictionary(store => store.Id, store => store);
 
             Items = searchResult.Results.Select<Member, IExportable>(member =>
             {
                 switch (member.MemberType)
                 {
                     case nameof(Contact):
-                        var contact = (Contact)member;
-                        var organizationId = contact.Organizations.FirstOrDefault();
-                        return new ExportableContact().FromModel(contact, organizationId != null ? allOrganizations[organizationId] : null);
+                        var contact = (Contact) member;
+                        var contactOrganizationId = contact.Organizations.OrderBy(organizationId => organizationId).FirstOrDefault();
+                        var account = contact.SecurityAccounts.OrderBy(account => account.Id).FirstOrDefault();
+                        var accountStoreId = account?.StoreId;
+                        return new ExportableContact().FromModel(contact, contactOrganizationId != null ? allOrganizations[contactOrganizationId] : null, accountStoreId != null ? stores[accountStoreId] : null);
                     case nameof(Organization):
-                        return new ExportableOrganization().FromModel((Organization)member);
+                        var organization = (Organization) member;
+                        var parentOrganizationId = organization.ParentId;
+                        return new ExportableOrganization().FromModel(organization, parentOrganizationId != null ? allOrganizations[parentOrganizationId] : null);
                     default:
                         throw new InvalidDataException();
                 }
--- src/VirtoCommerce.CustomerExportImportModule.Data/Services/CustomerExportPagedDataSourceFactory.cs
+++ src/VirtoCommerce.CustomerExportImportModule.Data/Services/CustomerExportPagedDataSourceFactory.cs
@@ -1,6 +1,7 @@
 using VirtoCommerce.CustomerExportImportModule.Core.Models;
 using VirtoCommerce.CustomerExportImportModule.Core.Services;
 using VirtoCommerce.CustomerModule.Core.Services;
+using VirtoCommerce.StoreModule.Core.Services;
 
 namespace VirtoCommerce.CustomerExportImportModule.Data.Services
 {
@@ -8,16 +9,18 @@ namespace VirtoCommerce.CustomerExportImportModule.Data.Services
     {
         private readonly IMemberService _memberService;
         private readonly IMemberSearchService _memberSearchService;
-        
-        public CustomerExportPagedDataSourceFactory(IMemberService memberService, IMemberSearchService memberSearchService)
+        private readonly IStoreService _storeService;
+
+        public CustomerExportPagedDataSourceFactory(IMemberService memberService, IMemberSearchService memberSearchService, IStoreService storeService)
         {
             _memberService = memberService;
             _memberSearchService = memberSearchService;
+            _storeService = storeService;
         }
         
         public ICustomerExportPagedDataSource Create(int pageSize, ExportDataRequest request)
         {
-            return new CustomerExportPagedDataSource(_memberService, _memberSearchService, pageSize, request);
+            return new CustomerExportPagedDataSource(_memberService, _memberSearchService, _storeService, pageSize, request);
         }
     }
 }

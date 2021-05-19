52c3335445b92b1e874182d0e403bb1f093c9882
Ilia <ilya.ivanov6@gmail.com>
2021-05-19 16:59:28 +0200

    Register toolbar button for import

--- src/VirtoCommerce.CustomerExportImportModule.Web/Scripts/module.js
+++ src/VirtoCommerce.CustomerExportImportModule.Web/Scripts/module.js
@@ -1,8 +1,34 @@
 // Call this to register your module to main application
-var moduleName = "virtoCommerce.customerExportImport";
+var moduleName = "virtoCommerce.customerExportImportModule";
 
 if (AppDependencies !== undefined) {
     AppDependencies.push(moduleName);
 }
 
-angular.module(moduleName, []);
+angular.module(moduleName, []).run(["virtoCommerce.featureManagerSubscriber", "platformWebApp.dialogService", "platformWebApp.toolbarService", "platformWebApp.authService", "virtoCommerce.customerModule.members", "virtoCommerce.customerModule.organizations",
+    function (featureManagerSubscriber, dialogService, toolbarService, authService, members, organizations) {
+        featureManagerSubscriber.onLoginStatusChanged(
+            "CustomerExportImport",
+            () => {
+
+                toolbarService.register({
+                    name: "platform.commands.import",
+                    icon: "fa fa-download",
+                    executeMethod: function (blade) {
+                        const newBlade = {
+                            id: 'customerImportFileUpload',
+                            title: 'customerExportImport.blades.file-upload.title',
+                            subtitle: 'customerExportImport.blades.file-upload.subtitle',
+                            controller: 'virtoCommerce.customerExportImportModule.fileUploadController',
+                            template: 'Modules/$(VirtoCommerce.CustomerExportImport)/Scripts/blades/file-upload.tpl.html'
+                        };
+                        bladeNavigationService.showBlade(newBlade, blade);
+                    },
+                    canExecuteMethod: function () { return true; },
+                    index: 4,
+                }, "virtoCommerce.customerModule.memberListController");
+
+            }
+        );
+    },
+]);

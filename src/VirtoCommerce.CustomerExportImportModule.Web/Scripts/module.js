0ae51d70f88f976ace400213794a134e61804d6e
Pavel Matantsev <matantsev.pn@gmail.com>
2021-05-20 16:23:49 +0300

    Add exportDataRequest and export resources

--- src/VirtoCommerce.CustomerExportImportModule.Web/Scripts/module.js
+++ src/VirtoCommerce.CustomerExportImportModule.Web/Scripts/module.js
@@ -1,76 +1,123 @@
 // Call this to register your module to main application
-var moduleName = "virtoCommerce.customerExportImportModule";
+var moduleName = 'virtoCommerce.customerExportImportModule';
 
 if (AppDependencies !== undefined) {
     AppDependencies.push(moduleName);
 }
 
-angular.module(moduleName, []).run(["virtoCommerce.featureManagerSubscriber", "platformWebApp.dialogService", "platformWebApp.toolbarService", "platformWebApp.authService",
-    function (featureManagerSubscriber, dialogService, toolbarService, authService) {
-        featureManagerSubscriber.onLoginStatusChanged(
-            "CustomerExportImport",
-            () => {
-                toolbarService.register({
-                    name: "platform.commands.import",
-                    icon: "fa fa-download",
-                    executeMethod: function (blade) {
-                        console.log(
-                            `test: ${this.name} ${this.icon} ${blade.title}`
-                        );
-                    },
-                    canExecuteMethod: function () {
-                        return true;
-                    },
-                    // permission: '',
-                    index: 4,
-                },
-                "virtoCommerce.customerModule.memberListController"
-                );
+angular.module(moduleName, []).run([
+    'virtoCommerce.featureManagerSubscriber',
+    'platformWebApp.dialogService',
+    'platformWebApp.toolbarService',
+    'virtoCommerce.customerModule.members',
+    'virtoCommerce.customerExportImportModule.export',
+    function (featureManagerSubscriber, dialogService, toolbarService, members, exportResources) {
+        featureManagerSubscriber.onLoginStatusChanged('CustomerExportImport', () => {
+            toolbarService.register(
+                {
+                    name: 'platform.commands.export',
+                    icon: 'fa fa-upload',
+                    executeMethod: async function (blade) {
+                        const scope = blade.$scope;
+                        const selection = scope.gridApi.selection;
+                        const keyword = blade.filter.keyword;
+                        const organizationId = blade.currentEntity.id;
+                        const exportDataRequest = {
+                            keyword,
+                            memberIds: [],
+                            organizationId
+                        };
 
-                toolbarService.register({
-                    name: "platform.commands.export",
-                    icon: "fa fa-upload",
-                    executeMethod: function (blade) {
-                        const selection = blade.$scope.gridApi.selection;
-                        if (!selection.getSelectedRows().length) {
-                            selection.selectAllRows();
+                        const getSearchCriteria = (memberType) => {
+                            return {
+                                memberType,
+                                memberId: organizationId,
+                                keyword: keyword || undefined,
+                                deepSearch: !!keyword,
+                                objectType: 'Member',
+                                take: 0
+                            };
+                        };
+
+                        const isAllSelected = !selection.getSelectedRows().length;
+
+                        if (isAllSelected) {
+                            members.search(getSearchCriteria('Contact'), (contactsSearchResponse) => {
+                                members.search(getSearchCriteria('Organization'), (organizationsSearchResponse) => {
+                                    const contactsQty = contactsSearchResponse.totalCount;
+                                    const organizationsQty = organizationsSearchResponse.totalCount;
+
+                                    const dialogData = {
+                                        isAllSelected,
+                                        contactsQty,
+                                        organizationsQty
+                                    };
+
+                                    showExportDialog(dialogData);
+                                });
+                            });
+                        } else {
+                            const selectedRows = selection.getSelectedRows();
+
+                            const selectedContactsList = _.filter(selectedRows, { memberType: 'Contact' });
+                            const selectedOrganizationsList = _.filter(selectedRows, { memberType: 'Organization' });
+                            const selectedMembersList = selectedContactsList.concat(selectedOrganizationsList);
+
+                            exportDataRequest.memberIds = _.pluck(selectedMembersList, 'id');
+
+                            const contactsQty = selectedContactsList.length;
+                            const organizationsQty = selectedOrganizationsList.length;
+
+                            const dialogData = {
+                                isAllSelected,
+                                contactsQty,
+                                organizationsQty
+                            };
+
+                            showExportDialog(dialogData);
                         }
-                        const isAllSelected = selection.getSelectAllState();
-                        const selectedRows = selection.getSelectedRows();
 
-                        const selectedOrganizationsList = _.filter(selectedRows, {memberType: "Organization"});
-                        const selectedContactsList = _.filter(selectedRows, {memberType: "Contact"});
+                        function showExportDialog({ isAllSelected, contactsQty, organizationsQty }) {
+                            const totalQty = contactsQty + organizationsQty;
+                            const exportIsEmpty = !totalQty;
+                            const dialog = {
+                                id: 'customerExportDialog',
+                                exportAll: isAllSelected,
+                                contactsQty,
+                                organizationsQty,
+                                totalQty,
+                                exportIsEmpty,
+                                callback: function (confirm) {
+                                    if (confirm) {
+                                        console.log('Confirmed');
+                                        console.log(exportDataRequest);
+                                        exportResources.run(exportDataRequest);
 
-                        const selectedOrganizationsCounter = selectedOrganizationsList.length;
-                        const selectedContactsCounter = selectedContactsList.length;
-                        const totalItemsCount = selectedOrganizationsCounter + selectedContactsCounter;
-                        const exportIsEmpty = !totalItemsCount;
-
-                        const dialog = {
-                            id: "customerExportDialog",
-                            exportAll: isAllSelected,
-                            selectedOrganizationsCounter,
-                            selectedContactsCounter,
-                            totalItemsCount,
-                            exportIsEmpty,
-                            callback: function (confirm) {
-                                if (confirm) {
-                                    console.log('Confirmed');
+                                        // blade.isExporting = true;
+                                        // var progressBlade = {
+                                        //     id: 'exportProgress',
+                                        //     title: 'export.blades.export-progress.title',
+                                        //     controller: 'virtoCommerce.exportModule.exportProgressController',
+                                        //     template: 'Modules/$(VirtoCommerce.Export)/Scripts/blades/export-progress.tpl.html',
+                                        //     exportDataRequest: exportDataRequest,
+                                        //     onCompleted: function () {
+                                        //         blade.isExporting = false;
+                                        //     }
+                                        // };
+                                        // bladeNavigationService.showBlade(progressBlade, blade);
+                                    }
                                 }
-                            }
+                            };
+                            dialogService.showDialog(dialog, 'Modules/$(VirtoCommerce.CustomerExportImport)/Scripts/dialogs/customerExport-dialog.tpl.html', 'platformWebApp.confirmDialogController');
                         }
-                        dialogService.showDialog(dialog, 'Modules/$(VirtoCommerce.CustomerExportImport)/Scripts/dialogs/customerExport-dialog.tpl.html', 'platformWebApp.confirmDialogController');
-
-                                },
+                    },
                     canExecuteMethod: function () {
                         return true;
                     },
-                    // permission: '',
-                    index: 5,
+                    index: 4
                 },
-                "virtoCommerce.customerModule.memberListController"
-                );
-            }
-        );
-    },
+                'virtoCommerce.customerModule.memberListController'
+            );
+        });
+    }
 ]);
---
+++ src/VirtoCommerce.CustomerExportImportModule.Web/Scripts/resources/resources.js
@@ -0,0 +1,9 @@
+﻿angular.module('virtoCommerce.customerExportImportModule')
+    .factory('virtoCommerce.customerExportImportModule.export', ['$resource', function ($resource) {
+        return $resource('api/customer/export', null,
+            {
+                run: { method: 'POST', url: 'api/customer/export/run'},
+                cancel: { method: 'POST', url: 'api/customer/export/cancel'}
+            });
+
+    }])

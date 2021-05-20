dc45b29864d14404223e3a24b8a3df0f922a3048
Ilia <ilya.ivanov6@gmail.com>
2021-05-20 10:45:45 +0200

    Add new blade for uploading csv file

--- src/VirtoCommerce.CustomerExportImportModule.Web/Scripts/module.js
+++ src/VirtoCommerce.CustomerExportImportModule.Web/Scripts/module.js
@@ -5,8 +5,8 @@ if (AppDependencies !== undefined) {
     AppDependencies.push(moduleName);
 }
 
-angular.module(moduleName, []).run(["virtoCommerce.featureManagerSubscriber", "platformWebApp.dialogService", "platformWebApp.toolbarService", "platformWebApp.authService", "virtoCommerce.customerModule.members", "virtoCommerce.customerModule.organizations",
-    function (featureManagerSubscriber, dialogService, toolbarService, authService, members, organizations) {
+angular.module(moduleName, []).run(["virtoCommerce.featureManagerSubscriber", "platformWebApp.bladeNavigationService", "platformWebApp.toolbarService",
+    function (featureManagerSubscriber, bladeNavigationService, toolbarService) {
         featureManagerSubscriber.onLoginStatusChanged(
             "CustomerExportImport",
             () => {
@@ -17,7 +17,8 @@ angular.module(moduleName, []).run(["virtoCommerce.featureManagerSubscriber", "p
                     executeMethod: function (blade) {
                         const newBlade = {
                             id: 'customerImportFileUpload',
-                            title: 'customerExportImport.blades.file-upload.title',
+                            title: (blade.currentEntity && blade.currentEntity.name) ? 'customerExportImport.blades.file-upload.title-member' : 'customerExportImport.blades.file-upload.title-root',
+                            titleValues: (blade.currentEntity && blade.currentEntity.name) && { member: blade.currentEntity.name },
                             subtitle: 'customerExportImport.blades.file-upload.subtitle',
                             controller: 'virtoCommerce.customerExportImportModule.fileUploadController',
                             template: 'Modules/$(VirtoCommerce.CustomerExportImport)/Scripts/blades/file-upload.tpl.html'

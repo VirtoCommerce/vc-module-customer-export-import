44b2226d3c69fdd60b8974c4ad23a9e0b39dee1c
Ilia <ilya.ivanov6@gmail.com>
2021-05-24 17:15:02 +0200

    VDS-1800: Fix validation message

--- src/VirtoCommerce.CustomerExportImportModule.Web/Scripts/blades/file-upload.js
+++ src/VirtoCommerce.CustomerExportImportModule.Web/Scripts/blades/file-upload.js
@@ -68,6 +68,18 @@ angular.module('virtoCommerce.customerExportImportModule')
             });
 
             uploader.onWhenAddingFileFailed = () => {
+                if ($scope.internalCsvError) {
+                    $scope.internalCsvError = false;
+                }
+
+                if (blade.csvFilePath) {
+                    assetsApi.remove({urls: [blade.csvFilePath]},
+                        () => { },
+                        (error) => bladeNavigationService.setError('Error ' + error.status, blade)
+                    );
+                    blade.csvFilePath = null;
+                }
+
                 $scope.showUploadResult = true;
             };
 

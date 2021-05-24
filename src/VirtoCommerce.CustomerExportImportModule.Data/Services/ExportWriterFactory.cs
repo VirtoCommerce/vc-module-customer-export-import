fe396d83b7aa83b0a891d7d3d2bdc638ae119039
Anton Veretennikov <superiorboroda@gmail.com>
2021-05-24 13:59:33 +0300

    VDS-1750 Implement an backend for the customers export (#13)
    * Draft ClassMap for contacts
    
    * Start make back for customers exporting
    
    * Implement Export Writer and its Factory.
    
    * Implement exporter and job. Register services
    
    * Build file paths by template
    
    * Debug
    
    * Format
    
    * Fix code smell
    
    * Fix errors in data source
    
    * Remove empty line
    
    * Take into account PR notes

---
+++ src/VirtoCommerce.CustomerExportImportModule.Data/Services/ExportWriterFactory.cs
@@ -0,0 +1,20 @@
+using CsvHelper.Configuration;
+using VirtoCommerce.CustomerExportImportModule.Core.Services;
+using VirtoCommerce.Platform.Core.Assets;
+
+namespace VirtoCommerce.CustomerExportImportModule.Data.Services
+{
+    public sealed class ExportWriterFactory : IExportWriterFactory
+    {
+        private readonly IBlobStorageProvider _blobStorageProvider;
+
+        public ExportWriterFactory(IBlobStorageProvider blobStorageProvider)
+        {
+            _blobStorageProvider = blobStorageProvider;
+        }
+        public IExportWriter<T> Create<T>(string filePath, Configuration csvConfiguration)
+        {
+            return new ExportWriter<T>(filePath, _blobStorageProvider, csvConfiguration);
+        }
+    }
+}

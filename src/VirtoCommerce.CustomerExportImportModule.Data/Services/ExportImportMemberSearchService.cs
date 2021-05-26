55b8b17e9d967430b3477b69b0ddf65b8f777ad1
Anton Veretennikov <superiorboroda@gmail.com>
2021-05-26 17:27:28 +0300

    Implemet recursive children getting

--- src/VirtoCommerce.CustomerExportImportModule.Data/Services/ExportImportMemberSearchService.cs
+++ src/VirtoCommerce.CustomerExportImportModule.Data/Services/ExportImportMemberSearchService.cs
@@ -1,11 +1,15 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using VirtoCommerce.CustomerModule.Core.Model;
 using VirtoCommerce.CustomerModule.Core.Model.Search;
 using VirtoCommerce.CustomerModule.Core.Services;
 using VirtoCommerce.CustomerModule.Core.Services.Indexed;
 using VirtoCommerce.CustomerModule.Data.Repositories;
 using VirtoCommerce.CustomerModule.Data.Services;
 using VirtoCommerce.Platform.Core.Caching;
+using VirtoCommerce.Platform.Core.Common;
 
 namespace VirtoCommerce.CustomerExportImportModule.Data.Services
 {
@@ -16,9 +20,58 @@ namespace VirtoCommerce.CustomerExportImportModule.Data.Services
         {
         }
 
-        public override Task<MemberSearchResult> SearchMembersAsync(MembersSearchCriteria criteria)
+        public override async Task<MemberSearchResult> SearchMembersAsync(MembersSearchCriteria criteria)
         {
-            return base.SearchMembersAsync(criteria);
+            var orgSkip = criteria.Skip;
+            var orgTake = criteria.Take;
+
+            criteria.Skip = 0;
+            criteria.Take = int.MaxValue;
+
+            var result = await base.SearchMembersAsync(criteria);
+
+            if (criteria.DeepSearch && (criteria.MemberId != null || !criteria.ObjectIds.IsNullOrEmpty() || criteria.Keyword != null))
+            {
+                var organizations = result.Results.OfType<Organization>().ToArray();
+                if (!organizations.IsNullOrEmpty())
+                {
+                    await LoadChildren(criteria, organizations, result);
+                }
+            }
+
+            //skip take as firstly
+            result.Results = result.Results.Skip(orgSkip).Take(orgTake).ToArray();
+
+            return result;
+        }
+
+        private async Task LoadChildren(MembersSearchCriteria criteria, IEnumerable<Organization> organizations, MemberSearchResult result)
+        {
+            foreach (var organization in organizations)
+            {
+                var searchChildrenCriteria = new MembersSearchCriteria()
+                {
+                    MemberId = organization.Id,
+                    DeepSearch = true,
+                    Skip = 0,
+                    Take = int.MaxValue,
+                    Sort = criteria.Sort,
+                    MemberTypes = criteria.MemberTypes,
+                    MemberType = criteria.MemberType
+                };
+
+                var searchChildrenResult = await RegularSearchMembersAsync(searchChildrenCriteria);
+
+                result.Results.AddRange(searchChildrenResult.Results);
+                result.TotalCount += searchChildrenResult.TotalCount;
+
+                var childOrganizations = searchChildrenResult.Results.OfType<Organization>().ToArray();
+
+                if (!childOrganizations.IsNullOrEmpty())
+                {
+                    await LoadChildren(criteria, childOrganizations, result);
+                }
+            }
         }
     }
 }

0e53aa053a376559a73635e673e5276d35c4598a
Ilia <ilya.ivanov6@gmail.com>
2021-05-27 16:38:23 +0200

    Truncate ids, add tooltips

--- src/VirtoCommerce.CustomerExportImportModule.Web/Scripts/blades/import-preview.js
+++ src/VirtoCommerce.CustomerExportImportModule.Web/Scripts/blades/import-preview.js
@@ -1,5 +1,5 @@
 angular.module('virtoCommerce.customerExportImportModule')
-    .controller('virtoCommerce.customerExportImportModule.importPreviewController', ['$scope', 'virtoCommerce.customerExportImportModule.import', '$filter', 'platformWebApp.bladeNavigationService', 'uiGridConstants', 'platformWebApp.uiGridHelper', 'platformWebApp.bladeUtils', 'platformWebApp.dialogService', '$translate', 'platformWebApp.settings', function ($scope, importResources, $filter, bladeNavigationService, uiGridConstants, uiGridHelper, bladeUtils, dialogService, $translate, settings) {
+    .controller('virtoCommerce.customerExportImportModule.importPreviewController', ['$scope', 'virtoCommerce.customerExportImportModule.import', 'platformWebApp.bladeNavigationService', 'uiGridConstants', 'platformWebApp.bladeUtils', function ($scope, importResources, bladeNavigationService, uiGridConstants, bladeUtils) {
         $scope.uiGridConstants = uiGridConstants;
 
         var blade = $scope.blade;
@@ -10,15 +10,29 @@ angular.module('virtoCommerce.customerExportImportModule')
             blade.isLoading = true;
             $scope.showUnparsedRowsWarning = false;
 
-            importResources.preview({ filePath: blade.csvFilePath }, (data) => {
-                _.each(data.results, record => {
+            importResources.preview({ filePath: blade.csvFilePath }, (response) => {
+                const records = response.results;
+
+                _.each(records, record => {
                     _.each(record.dynamicProperties, prop => {
                         _.extend(record, { [prop.name]: prop.values.map(value => value.value).join(', ') });
                     });
                     _.omit(record, 'dynamicProperties');
                 });
-                blade.currentEntities = data.results;
-                blade.totalCount = data.totalCount;
+
+                const columnNames = _.keys(records[0]);
+                const idColumnNames = _.filter(columnNames, key => key.includes('Id'));
+
+                $scope.originalRecords = _.map(records, record => ({...record}));
+
+                _.each(records, record => {
+                    _.each(idColumnNames, columnName => {
+                        record[columnName] = truncateId(record[columnName]);
+                    });
+                });
+
+                blade.currentEntities = records;
+                blade.totalCount = response.totalCount;
                 $scope.pageSettings.totalItems = 10;
                 getInvalidRowsCount();
                 blade.isLoading = false;
@@ -52,11 +66,11 @@ angular.module('virtoCommerce.customerExportImportModule')
                 gridApi.grid.registerDataChangeCallback((grid) => {
                     grid.buildColumns();
                     _.each(gridApi.grid.options.columnDefs, column => {
-                        column.cellTooltip = true;
+                        column.cellTooltip = getCellTooltip;
                         column.headerCellClass = 'br-0 font-weight-500 fs-13';
                     });
-                    const fullNameColumn = _.findWhere(gridApi.grid.options.columnDefs, {name: 'fullName'});
-                    const idColumn = _.findWhere(gridApi.grid.options.columnDefs, {name: 'id'});
+                    const fullNameColumn = _.findWhere(gridApi.grid.options.columnDefs, {name: 'contactFullName'});
+                    const idColumn = _.findWhere(gridApi.grid.options.columnDefs, {name: 'contactId'});
                     fullNameColumn.pinnedLeft = true;
                     fullNameColumn.cellClass = 'pl-7 bl-0 font-weight-500 fs-12';
                     fullNameColumn.headerCellClass = 'pl-7 font-weight-500 fs-13';
@@ -74,6 +88,21 @@ angular.module('virtoCommerce.customerExportImportModule')
                 $scope.unparsedRowsCount = $scope.previewCount - blade.currentEntities.length;
                 $scope.showUnparsedRowsWarning = true;
             }
+        }
+
+        function truncateId(content) {
+            if (content === null) return "";
+
+            if (content.length > 9) {
+                return content.substr(0, 3) + '...' + content.substr(content.length - 3, content.length);
+            }
+
+            return content;
+        }
+
+        function getCellTooltip(row, col) {
+            const index = blade.currentEntities.indexOf(row.entity);
+            return $scope.originalRecords[index][col.name];
         }
 
         initialize();

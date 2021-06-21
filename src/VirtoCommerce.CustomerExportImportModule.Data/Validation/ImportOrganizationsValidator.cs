e27b3a47b80dafe31396b7ad412268f7303c31fb
Anton Veretennikov <superiorboroda@gmail.com>
2021-06-21 17:56:30 +0300

    Add empty validator for organizations.

---
+++ src/VirtoCommerce.CustomerExportImportModule.Data/Validation/ImportOrganizationsValidator.cs
@@ -0,0 +1,18 @@
+using FluentValidation;
+using VirtoCommerce.CustomerExportImportModule.Core.Models;
+
+namespace VirtoCommerce.CustomerExportImportModule.Data.Validation
+{
+    public sealed class ImportOrganizationsValidator : AbstractValidator<ImportRecord<CsvOrganization>[]>
+    {
+        public ImportOrganizationsValidator()
+        {
+            AttachValidators();
+        }
+
+        private void AttachValidators()
+        {
+
+        }
+    }
+}

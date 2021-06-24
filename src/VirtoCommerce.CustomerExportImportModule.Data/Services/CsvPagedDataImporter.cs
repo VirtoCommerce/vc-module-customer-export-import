093eb24eb40138b7535ae3d6e722b2c622c545a7
Anton Veretennikov <superiorboroda@gmail.com>
2021-06-24 15:36:55 +0300

    Handle not closed case via missed columns callback

--- src/VirtoCommerce.CustomerExportImportModule.Data/Services/CsvPagedDataImporter.cs
+++ src/VirtoCommerce.CustomerExportImportModule.Data/Services/CsvPagedDataImporter.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using CsvHelper;
 using FluentValidation;
@@ -256,16 +257,71 @@ namespace VirtoCommerce.CustomerExportImportModule.Data.Services
                 return;
             }
 
-            var headerColumns = context.HeaderRecord;
-            var recordFields = context.Record;
-            var missedColumns = headerColumns.Skip(recordFields.Length).ToArray();
-            var error = $"This row has next missing columns: {string.Join(", ", missedColumns)}.";
-            var importError = new ImportError { Error = error, RawRow = context.RawRecord };
+            var importError = new ImportError();
+
+            if (!HandleIfItNotClosedQuote(context, importError))
+            {
+
+                var headerColumns = context.HeaderRecord;
+                var recordFields = context.Record;
+                var missedColumns = headerColumns.Skip(recordFields.Length).ToArray();
+                var error = $"This row has next missing columns: {string.Join(", ", missedColumns)}.";
+                importError.Error = error;
+                importError.RawRow = EscapeAloneQuotes(context.RawRecord);
+            }
 
             await reporter.WriteAsync(importError);
 
             errorsContext.ErrorsRows.Add(context.Row);
             HandleError(progressCallback, importProgress);
+        }
+
+        private static string EscapeAloneQuotes(string input)
+        {
+            return Regex.Replace(input, "([^\"])(\")([^\"])", "$1\\\"$3");
+        }
+
+        private static bool HandleIfItNotClosedQuote(ReadingContext context, ImportError importError)
+        {
+            if (context.CurrentIndex == 0)
+            {
+                return false;
+            }
+
+            var prevFieldValue = context.Record[context.CurrentIndex - 1];
+            var rawRecord = context.RawRecord;
+
+            var prevFieldInTheEndOfRaw = rawRecord.EndsWith(prevFieldValue);
+
+            if (prevFieldInTheEndOfRaw)
+            {
+
+                var symbolBeforePrevFieldValue = rawRecord.Substring(rawRecord.Length - prevFieldValue.Length - 1, 1);
+
+                if (symbolBeforePrevFieldValue == context.ParserConfiguration.Quote.ToString())
+                {
+                    importError.Error = "This row has invalid data. Quotes should be closed.";
+
+                    var indexOfSemicolon = prevFieldValue.IndexOf(";");
+                    var indexOfNewLine = prevFieldValue.IndexOf("\r\n");
+                    var indexOfNextSeparator = Math.Min(indexOfSemicolon, indexOfNewLine);
+
+                    var errorRawLostDataMarker = "\"...";
+
+                    if (indexOfNextSeparator > -1)
+                    {
+
+                        errorRawLostDataMarker = $"<quote was here>{prevFieldValue.Substring(0, indexOfNextSeparator)}<next data was lost>";
+                    }
+
+                    // -1 to remove quote
+                    importError.RawRow = rawRecord.Substring(0, rawRecord.Length - prevFieldValue.Length - 1) + errorRawLostDataMarker;
+
+                    return true;
+                }
+            }
+
+            return false;
         }
 
         private static void SetupErrorHandlers(Action<ImportProgressInfo> progressCallback, ImportConfiguration configuration,

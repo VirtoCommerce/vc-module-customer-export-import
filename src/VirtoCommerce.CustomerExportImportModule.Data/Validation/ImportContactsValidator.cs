e0568f1f2a49b2808483b5f981b55d0c400d31f3
Aleksandr Vishniakov <av@virtoway.com>
2021-07-05 13:31:59 +0200

    VDS-2060: Add validation and error messages for account (#44)
    * VDS-2060: Add validation and error messages for account
    
    * Fix code smell
    
    * Fix Store Id validation
    
    * Fix code smell
    
    * Fix Store Id invalid value when empty

--- src/VirtoCommerce.CustomerExportImportModule.Data/Validation/ImportContactsValidator.cs
+++ src/VirtoCommerce.CustomerExportImportModule.Data/Validation/ImportContactsValidator.cs
@@ -4,6 +4,8 @@ using VirtoCommerce.CustomerExportImportModule.Core.Models;
 using VirtoCommerce.Platform.Core.Common;
 using VirtoCommerce.Platform.Core.DynamicProperties;
 using VirtoCommerce.Platform.Core.Security;
+using VirtoCommerce.Platform.Core.Settings;
+using VirtoCommerce.StoreModule.Core.Services;
 
 namespace VirtoCommerce.CustomerExportImportModule.Data.Validation
 {
@@ -12,12 +14,17 @@ namespace VirtoCommerce.CustomerExportImportModule.Data.Validation
         private readonly ICountriesService _countriesService;
         private readonly IDynamicPropertyDictionaryItemsSearchService _dynamicPropertyDictionaryItemsSearchService;
         private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IStoreSearchService _storeSearchService;
+        private readonly ISettingsManager _settingsManager;
 
-        public ImportContactsValidator(ICountriesService countriesService, IDynamicPropertyDictionaryItemsSearchService dynamicPropertyDictionaryItemsSearchService, SignInManager<ApplicationUser> signInManager)
+        public ImportContactsValidator(ICountriesService countriesService, IDynamicPropertyDictionaryItemsSearchService dynamicPropertyDictionaryItemsSearchService, SignInManager<ApplicationUser> signInManager,
+            IStoreSearchService storeSearchService, ISettingsManager settingsManager)
         {
             _countriesService = countriesService;
             _dynamicPropertyDictionaryItemsSearchService = dynamicPropertyDictionaryItemsSearchService;
             _signInManager = signInManager;
+            _storeSearchService = storeSearchService;
+            _settingsManager = settingsManager;
 
             AttachValidators();
         }
@@ -26,7 +33,7 @@ namespace VirtoCommerce.CustomerExportImportModule.Data.Validation
         {
             RuleFor(importRecords => importRecords).SetValidator(_ => new ImportEntitiesAreNotDuplicatesValidator<ImportableContact>());
             RuleForEach(importRecords => importRecords).SetValidator(new ImportMemberValidator<ImportableContact>(_countriesService, _dynamicPropertyDictionaryItemsSearchService));
-            RuleForEach(importRecords => importRecords).SetValidator(new ImportContactValidator(_signInManager.UserManager));
+            RuleForEach(importRecords => importRecords).SetValidator(new ImportContactValidator(_signInManager.UserManager, _storeSearchService, _settingsManager));
         }
     }
 }
